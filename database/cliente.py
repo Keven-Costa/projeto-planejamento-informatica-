c7e213a454fe29578141379a6fbdf2d5fb012616
Keven-Costa <ogkeven278@gmail.com>
2024-06-10 15:25:50 -0300

    database e form atualizados

--- database/cliente.py
+++ database/cliente.py
@@ -1,5 +1,5 @@
 CLIENTES = [
-    {"id":1, "nome": "Izabela", "idade": 19, "sexo": "F", "altura": 1.7, "peso": 65.0, "pressao_arterial": 8, "alergias": "pó", "medicamentos_em_uso": "nenhum", "fuma": True, "bebe": True,
+    {"id":1, "nome": "Izabela", "idade": 19, "sexo": "F", "altura": 1.7, "peso": 65.0, "pressao_arterial": "10/6", "alergias": "Rinite", "medicamentos_em_uso": "nenhum", "fuma": True, "bebe": True,
      "parentes_com_doencas": "nenhum"}
    
 ]
